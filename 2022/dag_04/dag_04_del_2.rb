b92388dc6f53ee2bd0bc9e609a521c7b264f6621
Rilban <simon.rilbe@gmail.com>
2022-12-04 08:50:53 +0100

    dag 04

---
+++ 2022/dag_04/dag_04_del_2.rb
@@ -0,0 +1,24 @@
+input_file = File.open("dag_04_input.txt")
+file_data = input_file.readlines.map(&:chomp)
+
+
+contained_pairs = 0
+
+for n in file_data
+    first_range = n.split(",")[0]
+    second_range = n.split(",")[1]
+
+    first_range_start = first_range.split("-")[0].to_i
+    first_range_end = first_range.split("-")[1].to_i
+    second_range_start = second_range.split("-")[0].to_i
+    second_range_end = second_range.split("-")[1].to_i
+
+    if (first_range_start <= second_range_start && first_range_end >= second_range_end) || (second_range_start <= first_range_start && second_range_end >= first_range_end) || (first_range_start <= second_range_start && first_range_end >= second_range_start) || (second_range_start <= first_range_start && second_range_end >= first_range_start)
+        contained_pairs += 1
+    
+    end
+
+end
+
+puts contained_pairs
+
