51085ee21315c62d56d03a3500c14f4bcde74e9a
Rilban <simon.rilbe@gmail.com>
2022-12-04 08:52:17 +0100

    dag 04

--- 2022/dag_04/dag_04_del_2.rb
+++ 2022/dag_04/dag_04_del_2.rb
@@ -2,7 +2,7 @@ input_file = File.open("dag_04_input.txt")
 file_data = input_file.readlines.map(&:chomp)
 
 
-contained_pairs = 0
+overlapping_pairs = 0
 
 for n in file_data
     first_range = n.split(",")[0]
@@ -14,11 +14,10 @@ for n in file_data
     second_range_end = second_range.split("-")[1].to_i
 
     if (first_range_start <= second_range_start && first_range_end >= second_range_end) || (second_range_start <= first_range_start && second_range_end >= first_range_end) || (first_range_start <= second_range_start && first_range_end >= second_range_start) || (second_range_start <= first_range_start && second_range_end >= first_range_start)
-        contained_pairs += 1
-    
+        overlapping_pairs += 1
     end
 
 end
 
-puts contained_pairs
+puts overlapping_pairs
 
