a776eaba0ac358498c0a76187e813f3853e8c33c
Rilban <simon.rilbe@gmail.com>
2022-12-06 12:42:46 +0100

    dag 06

---
+++ 2022/dag_06/dag_06_del_2.rb
@@ -0,0 +1,28 @@
+input = File.read("dag_06_input.txt")
+
+
+marker_found = false
+marker = input[0..13]
+i = 3
+
+
+while i < input.length
+    marker.each_char { |char| 
+        if marker.count(char) > 1
+            marker_found = false
+            break
+        else
+            marker_found = true
+        end
+        
+    }
+    
+    if marker_found
+        p i + 1
+        break
+    end
+
+    marker[0] = ""
+    i += 1
+    marker << input[i]
+end
